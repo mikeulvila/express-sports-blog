230fbe7da462aadaeeea8d3df9cf1e1eefcc3b46
Mike Ulvila <mikeulvila@me.com>
2016-02-11 09:37:37 -0600

    articles now populating to index from database, removed extra res.render

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-Article = require('../models/article.js')
+var Article = require('../models/article.js')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Article.getArticles(function (err, articles) {
+    console.log('articles', articles);
     if (err) {
       res.send(err);
     } else {
@@ -14,7 +15,6 @@ router.get('/', function(req, res, next) {
       })
     }
   });
-  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
